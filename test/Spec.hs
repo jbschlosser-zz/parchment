ef8e00eb44bc37db5a0874c204579ab41bac38fa
Joel Schlosser <jbschlosser@gmail.com>
2017-05-17 21:05:20 -0400

    Receive data profiling test code

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,22 +2,25 @@
 
 import Control.Concurrent.STM.TQueue
 import Control.Monad (void)
+import qualified Data.ByteString as BS
 import Data.Map (fromList)
+import Data.Word (Word8)
+import Language.Scheme.Core
+import Language.Scheme.Types hiding (bindings)
 import Lens.Micro ((.~), (^.), (&), (%~), over, ix, Getting)
 import Lens.Micro.TH (makeLenses)
-import Parchment.Fchar
-import Parchment.Parsing
-import Parchment.Session
+import Parchment.FString
+import Parchment.Session hiding (cursor)
 import Test.HUnit
 
-makeLenses ''Sess
-
-fakeHistory = ["this", "is", "a", "test"]
+--makeLenses ''Sess
 
 fakeSession :: IO Sess
 fakeSession = do
     queue <- newTQueueIO
-    let sess = initialSession queue $ fromList []
+    scmEnv <- r5rsEnv
+    let settings = defaultSettings "localhost" 4000
+    let sess = initialSession settings queue (fromList []) scmEnv
     return sess
 
 unchangedState :: (Eq a, Show a) => s -> s -> String -> Getting a s a -> Assertion
@@ -27,18 +30,27 @@ changedState :: (Eq a, Show a) => s -> s -> String -> Getting a s a -> (a -> a) 
 changedState initial final desc getter transform  =
     assertEqual desc (transform $ initial ^. getter) (final ^. getter)
 
-test1 = TestLabel "History properly moves forward" $ TestCase $ do
+testBS :: BS.ByteString
+testBS = BS.pack . concat . take 5000 . repeat $ ([65, 66, 67, 68, 69, 70] :: [Word8])
+
+-- test1 = TestLabel "History properly moves forward" . TestCase $ do
+--     sess <- fakeSession
+--     let initial = sess
+--          & history .~ fakeHistory
+--          & history_loc .~ 3
+--          & cursor .~ 0
+--     let final = nextHistory initial
+--     let changed = changedState initial final
+--     let unchanged = unchangedState initial final
+--     unchanged "Cursor" cursor
+--     changed "History" history (flip (++) [""])
+--     changed "History loc" history_loc (+1)
+
+test1 = TestLabel "Benchmarking receive" . TestCase $ do
     sess <- fakeSession
     let initial = sess
-         & history .~ fakeHistory
-         & history_loc .~ 3
-         & cursor .~ 0
-    let final = nextHistory initial
-    let changed = changedState initial final
-    let unchanged = unchangedState initial final
-    unchanged "Cursor" cursor
-    changed "History" history (flip (++) [""])
-    changed "History loc" history_loc (+1)
+    let final = receiveServerData initial testBS
+    assertEqual "Reflexive" (final ^. (recv_state . text)) (final ^. (recv_state . text))
 
 tests = TestList
     [ test1
